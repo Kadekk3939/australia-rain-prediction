7e402f65a370c18d2c4ebebd9c88ea948015820e
Kadekk3939 <kadekk3939@gmail.com>
2023-06-04 15:16:15 +0200

    Added name to container

--- run_container.bat
+++ run_container.bat
@@ -1,3 +1,3 @@
 @REM docker build -t biai-project .
-docker run --rm -it -p 8888:8888 -v ${PWD}/working:/working biai-project
+docker run --rm -it -p 8888:8888 -v ${PWD}/working:/working --name biai-project-container biai-project
 pause
