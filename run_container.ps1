2f6313c3fc8ebf63d32b2b6b94da3b819a9422db
Kadekk3939 <kadekk3939@gmail.com>
2023-06-26 15:55:30 +0200

    Notebook now opens in default browser

--- run_container.ps1
+++ run_container.ps1
@@ -6,7 +6,7 @@
 # Run container
 Start-Job {
     Start-Sleep -Seconds 4
-    Start-Process chrome '--start-fullscreen "http://localhost:8888"'
+    Start-Process '--start-fullscreen "http://localhost:8888"'
 } -Name 'notebook'
 
 docker run --rm -it -p 8888:8888 -v ${PWD}/working:/working --name biai-project-container biai-project
